268d0b35ab7c0a8b51ba1fa70eb35c7a4cd7a318
minecraft195 <76690513+minecraft195@users.noreply.github.com>
2021-10-06 19:00:25 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+var first_name = "Anantshree"
+var last_name = "nothing"
+
+var no_1 = 10
+var no_2 = 5
+
+var full_name=first_name.concat(last_name)
+
+console.log(full_name)
+
+var add = no_1+no_2
+
+console.log(add)
